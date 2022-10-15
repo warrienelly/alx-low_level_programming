135b850a9c1c645447fe1384529830c72a59be4e
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-15 01:43:28 +0100

    Added calculation for positive and negative

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Print - Size of various datatype of computer it is compiled on
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (a = 0; a < b; a++)
+		printf("%d\n", a);
+
+	return (0);
+}
