03eec99ab231afa267430e44278ee2a111d21e56
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-19 00:07:30 +0100

    Removing main from file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,19 +4,14 @@
  *
  * Return: 0
  */
-int main(void)
-
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'A'; i++)
+	{
 		_putchar(i);
 		_putchar('\n');
+	}
 
 }
