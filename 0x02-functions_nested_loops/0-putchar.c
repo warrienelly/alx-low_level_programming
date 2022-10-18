8b2a00474eabe9a138f6a9ec957d1eda204d31db
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-18 19:47:08 +0100

    Making new changs to the file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print putchar from the function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
