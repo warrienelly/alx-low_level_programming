fcef9ee197bdcc48071abb1bfd9d095a7b05fd81
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-18 23:54:08 +0100

    making use of KC function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print alphabet from the function stated
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	for (i = 'a'; i <= 'A'; i++)
+		_putchar(i);
+		_putchar('\n);
+
+}
