67bd19f279dd11fb427e122cbe87755b5a466d22
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-20 19:06:12 +0100

    New addition oo

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,11 @@ int print_last_digit(int n)
 
 	if (n < 0)
 		n = -1 * n;
-
 	last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit = -1 * last_digit;
+ 
 	_putchar('0' + last_digit);
 	return (last_digit);
 }
