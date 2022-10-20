da0a8ddf542935406b582cf91739c27fe6754420
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-20 18:46:57 +0100

    Making changes to the multiplication

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <limits.h>
 /**
  * main - check the code
  *
@@ -11,7 +12,8 @@ int main(void)
 
     print_last_digit(98);
     print_last_digit(0);
-    r = print_last_digit(-1024);
+    r = print_last_digit(INT_MIN);
+    printf("%d", r);
     _putchar('0' + r);
     _putchar('\n');
     return (0);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,8 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = n * -1;
+		n = -1 * 10;
+
 	last_digit = n % 10;
 	_putchar('0' + last_digit);
 	return (last_digit);
