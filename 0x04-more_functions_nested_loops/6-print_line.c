2a90215c9e45ea5c09526234dbb0bedf740255c1
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-22 02:25:20 +0100

    print line code added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draw a straight line in the terminal
+ * @n: the number of times the character _ should be printed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void more_numbers(void);
 void print_most_numbers(void);
 int _putchar(char c);
