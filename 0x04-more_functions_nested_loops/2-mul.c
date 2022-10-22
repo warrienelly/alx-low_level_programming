d800e6879d7ac047f5dcc7f61e3b4855a7994fda
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-22 01:22:49 +0100

    function to multiply 2 integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+
+/**
+  * mul - multiplies 2 interger 
+  *
+  * @a: first integer to muliple
+  * @b: second integer to multiple
+  * Return: interger
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
