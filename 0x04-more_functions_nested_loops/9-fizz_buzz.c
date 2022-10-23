a070231621fd0d3ff083030b348ee1bf7cabc7d1
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-23 23:27:24 +0100

    making new changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,25 +8,26 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
-		}
-		if ((i % 5) == 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
-		}
-
-		if ((i % 3) != 0 && (i % 5) != 0)
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
 		{
 			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
 		}
-		if (i != 100)
-			printf(" ");
 	}
 	printf("\n");
 
