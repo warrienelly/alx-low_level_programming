5880f1faefc1c6241f88152ffa20d1686e66eb8c
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-24 23:51:44 +0100

    Battery st started code

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - function that takes a pointer to a int as parameter
+ * and updates it points to 98
+ * @n: the value to collect
+ *
+ * Return: Always void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
