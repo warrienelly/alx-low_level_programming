fc361fb3f80c25ad463eb0ace023fb709eed7678
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-25 23:31:58 +0100

    New changes to the file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,16 @@
 /**
  * _strlen - function that returns the length of a string.
  * @s: the string
- * 
+ *
  *
  * Return: Always interger.
  */
 int _strlen(char *s)
 {
-	
 	int i;
 	int sum = 0;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; *s; i++)
 	{
 		s++;
 		sum++;
