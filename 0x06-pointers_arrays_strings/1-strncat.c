875100e5b467f3a4bbe2187f8e43b9f3e7da378a
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-27 23:00:48 +0100

    Concatenate string with number

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncat - appends the src string to the dest string,
+ * it will use at most n bytes from src; and
+ * src does not need to be null-terminated 
+ * if it contains n or more bytes.
+ *
+ * @dest: resulting string
+ * @src: appending string
+ * @n: n most byte to return.
+ * 
+ * Return: Always pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i;
+	int j;
+	int len = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		if (j <= (n-1))
+		{
+			*(dest + len) = src[j];
+		}
+		j++;
+		len++;
+	}
+	return (dest);
+
+}
+
