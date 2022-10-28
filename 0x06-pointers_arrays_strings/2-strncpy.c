bf54f6f7a761c3c27eb823bc031d9949beae7ab8
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-28 02:31:49 +0100

    New changes with fixing errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,18 +13,24 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 
-	int i;
+	int i = 0;
+
+	int len = 0;
+
+	while (src[i] != '\0')
+	{
+		len++;
+		i++;
+	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		*(dest + i) = src[i];
 	}
 
-	if (i <  n)
+	for (i = len; i < n; i++)
 	{
 		*(dest + i) = '\0';
-		i++;
-
 	}
 	return (dest);
 
