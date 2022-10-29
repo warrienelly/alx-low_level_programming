0a742912c34aa2b2287c88c170b3515d83bf3818
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-29 18:31:15 +0100

    fixing betty issues

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,29 +6,27 @@
  *  Letters t and T should be replaced by 7
  *  Letters l and L should be replaced by 1
  * @s: string values
- * 
+ *
  * Return: Always Pointer
  */
 char *leet(char *s)
 {
-    int leet_count;
-    char leet[] = "aAeEoOtTlL";
-    char leet_no[] = "4433007711";
+	int leet_count;
 
-    int char_count = 0;
+	char leet[] = "aAeEoOtTlL";
+	char leet_no[] = "4433007711";
 
-    while (s[char_count] != '\0')
-    {
-        for (leet_count = 0; leet_count < 10; leet_count++)
-        {
-            if (leet[leet_count] == s[char_count])
-            {
-                s[char_count] = leet_no[leet_count];
-            }
-        }
-        char_count++;
-    }
+	int char_count = 0;
 
-    return (s);
+	while (s[char_count] != '\0')
+	{
+		for (leet_count = 0; leet_count < 10; leet_count++)
+		{
+			if (leet[leet_count] == s[char_count])
+				s[char_count] = leet_no[leet_count];
+		}
+	char_count++;
+	}
 
+	return (s);
 }
