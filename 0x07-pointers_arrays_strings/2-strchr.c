9ad4d867319552774685b312353cd5171287fbd3
Warrie Warrie <warrie.warrieus@gmail.com>
2022-11-04 01:23:50 +0100

    Replace 0 with null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * *_strchr - function that returns first occurrence of
  * the character c in the string s,
@@ -19,5 +20,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
