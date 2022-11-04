0e1e65b02e177990637211ccc61566875dc58c48
Warrie Warrie <warrie.warrieus@gmail.com>
2022-11-04 01:18:24 +0100

    code for str char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+/**
+ * *_strchr - function that returns first occurrence of
+ * the character c in the string s,
+ * or NULL if the character is not found
+ *
+ * @s: string to work on
+ * @c: character to check on string
+ *
+ * Return: Always pointer to c
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (0);
+}
