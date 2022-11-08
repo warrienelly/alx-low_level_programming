f0e42d8196dcaa0f001ede1a38344bb02a231c5e
Warrie Warrie <warrie.warrieus@gmail.com>
2022-11-08 23:28:37 +0100

    Making new filechanges

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,11 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int counter = 0;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	ptr = malloc(sizeof(c) * size);
 	if (ptr == NULL)
 	{
