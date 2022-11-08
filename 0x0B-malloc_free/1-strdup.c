8304ffa2e71eae62e772dc2fbfe547004e10b913
Warrie Warrie <warrie.warrieus@gmail.com>
2022-11-09 00:31:22 +0100

    code for duplicating string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,50 @@
+/**
+ * _strlen - find the length of a string
+ * @str: string to be found
+ *
+ * Return: Integer
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+#include <stdlib.h>
+/**
+ * *_strdup - function that returns a pointer to a new string which
+ * is a duplicate of the string str. Memory for the new string with
+ * malloc, and can be freed with free.
+ *
+ * @str: pointer of assigning string
+ *
+ * Return: Always pointer or NULL
+ */
+char *_strdup(char *str)
+{
+	int len;
+	char *ptr;
+
+	int i = 0;
+
+	len = _strlen(str);
+	ptr = malloc(sizeof(*str) * len);
+
+	if (str == NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		*(ptr + i) = *(str + i);
+		i++;
+	}
+	*(ptr + i) = '\0';
+	return (ptr);
+
+}
